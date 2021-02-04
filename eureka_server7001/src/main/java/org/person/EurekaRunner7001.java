2fae02475a21fa7878c303eff63c684d415e1317
RainbowPerfect <903933616@qq.com>
2021-02-05 00:07:16 +0800

    添加eureka的7001端口

--- eureka_server7001/src/main/java/org/person/EurekaRunner7001.java
+++ eureka_server7001/src/main/java/org/person/EurekaRunner7001.java
@@ -2,19 +2,20 @@ package org.person;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * 启动类
+ * 7001启动类
  *
  * @author rainbowPerfect/RainbowPerfect
  * @version v1.0
- * @create 2021/1/19/0:15
+ * @create 2021/2/4/23:27
  */
+
 @SpringBootApplication
-@EnableEurekaClient
-public class ProviderRunner8001 {
+@EnableEurekaServer
+public class EurekaRunner7001 {
     public static void main(String[] args) {
-        SpringApplication.run(ProviderRunner8001.class);
+        SpringApplication.run(EurekaRunner7001.class);
     }
 }
